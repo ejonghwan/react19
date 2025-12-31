59c9c1cb670a1075da12ece82b872536c701a06f
ejonghwan <sun87-1@daum.net>
2025-12-31 17:52:17 +0900

    a

--- src/ani/ani.tsx
+++ src/ani/ani.tsx
@@ -16,6 +16,28 @@ import img_03 from '../../public/img01/img_03.png';
 import img_04 from '../../public/img01/img_04.png';
 import img_05 from '../../public/img01/img_05.png';
 import img_06 from '../../public/img01/img_06.png';
+import img_07 from '../../public/img01/img_07.png';
+import img_08 from '../../public/img01/img_08.png';
+import img_icon1_1 from '../../public/img01/icon_1/1.png';
+import img_icon1_2 from '../../public/img01/icon_1/2.png';
+import img_icon1_3 from '../../public/img01/icon_1/3.png';
+import img_icon1_4 from '../../public/img01/icon_1/4.png';
+import img_icon1_5 from '../../public/img01/icon_1/5.png';
+import img_icon1_6 from '../../public/img01/icon_1/6.png';
+
+import img_icon2_1 from '../../public/img01/icon_2/1.png';
+import img_icon2_2 from '../../public/img01/icon_2/2.png';
+import img_icon2_3 from '../../public/img01/icon_2/3.png';
+import img_icon2_4 from '../../public/img01/icon_2/4.png';
+import img_icon2_5 from '../../public/img01/icon_2/5.png';
+import img_icon2_6 from '../../public/img01/icon_2/6.png';
+
+import img_icon3_1 from '../../public/img01/icon_3/1.png';
+import img_icon3_2 from '../../public/img01/icon_3/2.png';
+import img_icon3_3 from '../../public/img01/icon_3/3.png';
+import img_icon3_4 from '../../public/img01/icon_3/4.png';
+import img_icon3_5 from '../../public/img01/icon_3/5.png';
+import img_icon3_6 from '../../public/img01/icon_3/6.png';
 
 
 
@@ -74,36 +96,78 @@ const Ani = () => {
                                 <img src={img_06} alt="" />
                             </div>
                         </div>
+                        <div className={clsx(style["ani__wrap--item"], style["ani_1_6_tit"])}>
+                            <img src={img_07} alt="" className={style["img_07"]} />
+                        </div>
                         <div className={clsx(style["ani__wrap--item"], style["ani_1_6"])}>
-                            <div className={clsx(style["ani__wrap--item"], style["ani_1_6_1"])}>
-                                <div>icon1_1</div>
-                                <div>icon1_2</div>
-                                <div>icon1_3</div>
-                            </div>
-                            <div className={clsx(style["ani__wrap--item"], style["ani_1_6_2"])}>
-                                <div>icon2_1</div>
-                                <div>icon2_2</div>
-                                <div>icon2_3</div>
-                            </div>
-                            <div className={clsx(style["ani__wrap--item"], style["ani_1_6_3"])}>
-                                <div>icon3_1</div>
-                                <div>icon3_2</div>
-                                <div>icon3_3</div>
-                            </div>
-                            <div className={clsx(style["ani__wrap--item"], style["ani_1_6_4"])}>
-                                <div>icon4_1</div>
-                                <div>icon4_2</div>
-                                <div>icon4_3</div>
-                            </div>
-                            <div className={clsx(style["ani__wrap--item"], style["ani_1_6_5"])}>
-                                <div>icon5_1</div>
-                                <div>icon5_2</div>
-                                <div>icon5_3</div>
-                            </div>
-                            <div className={clsx(style["ani__wrap--item"], style["ani_1_6_6"])}>
-                                <div>icon6_1</div>
-                                <div>icon6_2</div>
-                                <div>icon6_3</div>
+                            <img src={img_08} alt="" className={style["img_08"]} />
+                            <div className={style["icon_wrap"]}>
+                                <div className={clsx(style["ani__wrap--item"], style["ani_1_6_1"])}>
+                                    <div>
+                                        <img src={img_icon1_1} alt="" />
+                                    </div>
+                                    <div>
+                                        <img src={img_icon2_1} alt="" />
+                                    </div>
+                                    <div>
+                                        <img src={img_icon3_1} alt="" />
+                                    </div>
+                                </div>
+                                <div className={clsx(style["ani__wrap--item"], style["ani_1_6_2"])}>
+                                    <div>
+                                        <img src={img_icon1_2} alt="" />
+                                    </div>
+                                    <div>
+                                        <img src={img_icon2_2} alt="" />
+                                    </div>
+                                    <div>
+                                        <img src={img_icon3_2} alt="" />
+                                    </div>
+                                </div>
+                                <div className={clsx(style["ani__wrap--item"], style["ani_1_6_3"])}>
+                                    <div>
+                                        <img src={img_icon1_3} alt="" />
+                                    </div>
+                                    <div>
+                                        <img src={img_icon2_3} alt="" />
+                                    </div>
+                                    <div>
+                                        <img src={img_icon3_3} alt="" />
+                                    </div>
+                                </div>
+                                <div className={clsx(style["ani__wrap--item"], style["ani_1_6_4"])}>
+                                    <div>
+                                        <img src={img_icon1_4} alt="" />
+                                    </div>
+                                    <div>
+                                        <img src={img_icon2_4} alt="" />
+                                    </div>
+                                    <div>
+                                        <img src={img_icon3_4} alt="" />
+                                    </div>
+                                </div>
+                                <div className={clsx(style["ani__wrap--item"], style["ani_1_6_5"])}>
+                                    <div>
+                                        <img src={img_icon1_5} alt="" />
+                                    </div>
+                                    <div>
+                                        <img src={img_icon2_5} alt="" />
+                                    </div>
+                                    <div>
+                                        <img src={img_icon3_5} alt="" />
+                                    </div>
+                                </div>
+                                <div className={clsx(style["ani__wrap--item"], style["ani_1_6_6"])}>
+                                    <div>
+                                        <img src={img_icon1_6} alt="" />
+                                    </div>
+                                    <div>
+                                        <img src={img_icon2_6} alt="" />
+                                    </div>
+                                    <div>
+                                        <img src={img_icon3_6} alt="" />
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
